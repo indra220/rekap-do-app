d022841237d4e5944db943534c64b0e337e6ff53
indra220 <m.indra.p42@gmail.com>
2026-06-09 00:07:56 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // WAJIB UNTUK ELECTRON: 
+  // Memaksa Next.js untuk mem-build file menjadi HTML statis di dalam folder 'out'
+  output: 'export',
+  
+  // Wajib ditambahkan saat menggunakan output: 'export' 
+  // karena fitur optimasi gambar bawaan server Next.js tidak didukung pada mode statis desktop
+  images: {
+    unoptimized: true,
+  },
+
   // Mengizinkan akses Hot Module Replacement (HMR) dari IP lokal untuk Electron
   allowedDevOrigins: [
     '127.0.0.1',
     '192.168.1.13',
     'localhost'
   ],
-  
-  // Jika Anda sudah memiliki konfigurasi nextConfig lainnya sebelumnya, 
-  // Anda bisa menggabungkannya di dalam object ini.
 };
 
 export default nextConfig;
